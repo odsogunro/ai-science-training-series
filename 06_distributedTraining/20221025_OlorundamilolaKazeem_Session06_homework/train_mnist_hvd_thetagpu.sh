c42966ce9b885165803bc2cc7a0726f3bf0a0862
olorundamilola kazeem <dami.kazeem@gmail.com>
2022-11-22 06:17:44 +0000

    updated session 06 and fully runs on bigmem again

--- 06_distributedTraining/20221025_OlorundamilolaKazeem_Session06_homework/train_mnist_hvd_thetagpu.sh
+++ 06_distributedTraining/20221025_OlorundamilolaKazeem_Session06_homework/train_mnist_hvd_thetagpu.sh
@@ -19,3 +19,4 @@ mpirun -n 1 python tensorflow2_mnist_hvd.py
 mpirun -n 2 python tensorflow2_mnist_hvd.py
 mpirun -n 4 python tensorflow2_mnist_hvd.py
 mpirun -n 8 python tensorflow2_mnist_hvd.py
+# mpirun -n 16 python tensorflow2_mnist_hvd.py
